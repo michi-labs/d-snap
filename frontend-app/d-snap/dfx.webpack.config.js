fe6141db6928b8cdd5a9cf261895d51b6df9d7e1
Adrián Darío Hidalgo Flores <adrian@michilabs.io>
2023-12-07 15:40:28 -0600

    general fixes

--- frontend-app/d-snap/dfx.webpack.config.js
+++ frontend-app/d-snap/dfx.webpack.config.js
@@ -1,27 +1,18 @@
 const path = require("path")
 
-let localCanisters, prodCanisters, canisters
-
 function initCanisterIds() {
+    let canisters;
+
     try {
-        localCanisters = require(path.resolve("../../dfx", "local", "canister_ids.json"))
+        canisters = require(path.resolve("../../.dfx", "local", "canister_ids.json"));
     } catch (error) {
-        console.log("No local canister_ids.json found. Continuing production")
-    }
-    try {
-        prodCanisters = require(path.resolve("canister_ids.json"))
-    } catch (error) {
-        console.log("No production canister_ids.json found. Continuing with local")
+        console.log("No production canister_ids.json found. Continuing with local");
     }
 
-    const network =
-        process.env.DFX_NETWORK ||
-        (process.env.NODE_ENV === "production" ? "ic" : "local")
+    const network = process.env.DFX_NETWORK || 'local';
 
-    console.info(`initCanisterIds: network=${network}`)
-    console.info(`initCanisterIds: DFX_NETWORK=${process.env.DFX_NETWORK}`)
-
-    canisters = network === "local" ? localCanisters : prodCanisters
+    console.info(`initCanisterIds: network=${network}`);
+    console.info(`initCanisterIds: DFX_NETWORK=${process.env.DFX_NETWORK}`);
 
     for (const canister in canisters) {
         process.env[`NEXT_PUBLIC_${canister.toUpperCase()}_CANISTER_ID`] =
--- frontend-app/d-snap/next.config.js
+++ frontend-app/d-snap/next.config.js
@@ -1,5 +1,4 @@
-/** @type {import('next').NextConfig} */
-const DFXWebPackConfig = require("./dfx.webpack.config")
+const DFXWebPackConfig = require("./dfx.webpack.config");
 DFXWebPackConfig.initCanisterIds();
 
 const webpack = require("webpack")
@@ -7,7 +6,7 @@ const webpack = require("webpack")
 // Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
 const EnvPlugin = new webpack.EnvironmentPlugin({
     DFX_NETWORK: "local"
-})
+});
 
 module.exports = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
