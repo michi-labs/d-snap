31f577da56fab7eea56562d66edaeab3860ecfb3
Adrián Darío Hidalgo Flores <adrian@michilabs.io>
2023-12-08 13:21:03 -0600

    prevent infinite loops

--- frontend/apps/webapp/src/pages/index.tsx
+++ frontend/apps/webapp/src/pages/index.tsx
@@ -5,11 +5,13 @@ import { AuthButton } from "../components/auth/auth-button";
 
 export default function HomePage() {
   const [greet, setGreet] = useState("");
+  const [username, setUsername] = useState("");
   const test = useActor("test");
+  const user = useActor("user");
 
   useEffect(() => {
     getGreet();
-  });
+  }, []);
 
   async function getGreet() {
     // @ts-ignore
@@ -17,10 +19,35 @@ export default function HomePage() {
     setGreet(greet);
   }
 
+  async function create() {
+    try {
+      // @ts-ignore
+      await user.create({
+        username: "adrian_hidalgo",
+        bio: "Software developer",
+        picture: {
+          url: "https://www.google.com",
+        },
+      });
+
+      // @ts-ignore
+      const profile = await user.getProfile();
+      console.log({ profile });
+
+      setUsername(profile.username);
+    } catch (error) {
+      console.log({ error });
+    }
+  }
+
   return (
     <div>
       <h1>Home: {greet}</h1>
       <AuthButton />
+      <button onClick={() => create()}>Crear</button>
+
+      <div>Profile</div>
+      <div>Username: {username}</div>
     </div>
   );
 }
