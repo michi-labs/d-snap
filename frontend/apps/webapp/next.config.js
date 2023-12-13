a3458ebd52776cb93bcbc200b2cca20810f64101
Adrián Darío Hidalgo Flores <adrian@michilabs.io>
2023-12-13 12:12:07 -0600

    next-transpile-modules removed

--- frontend/apps/webapp/next.config.js
+++ frontend/apps/webapp/next.config.js
@@ -1,23 +1,21 @@
 const webpack = require("webpack");
-const transpileModules = require('next-transpile-modules');
 
 const DFXWebPackConfig = require("./dfx.webpack.config");
 DFXWebPackConfig.bootstrap("../../..");
-
-const withTM = transpileModules(['icp-connect-core', 'icp-connect-react']);
 
 // Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
 const EnvPlugin = new webpack.EnvironmentPlugin({
     DFX_NETWORK: "local",
 });
 
-module.exports = withTM({
+module.exports = {
     webpack: (config) => {
         // Plugin
         config.plugins.push(EnvPlugin);
 
         return config;
     },
+    transpilePackages: ["icp-connect-core", "icp-connect-react"],
     output: "export",
     distDir: "build",
-});
+};
