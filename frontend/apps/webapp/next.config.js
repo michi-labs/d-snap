a1ee10cb32b2d229f338ce080df34edc20110fb1
Alan <alannnc@gmail.com>
2023-12-13 00:13:12 -0700

    Fix build with next and expo

--- frontend/apps/webapp/next.config.js
+++ frontend/apps/webapp/next.config.js
@@ -1,6 +1,7 @@
 const DFXWebPackConfig = require("./dfx.webpack.config");
 DFXWebPackConfig.bootstrap("../../..");
 
+const { withExpo } = require("@expo/next-adapter");
 const webpack = require("webpack");
 
 // Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
@@ -8,13 +9,14 @@ const EnvPlugin = new webpack.EnvironmentPlugin({
   DFX_NETWORK: "local",
 });
 
-module.exports = {
+module.exports = withExpo({
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Plugin
     config.plugins.push(EnvPlugin);
 
     return config;
   },
+  transpilePackages: ["expo-web-browser", "expo-secure-store"],
   output: "export",
   distDir: "build",
-};
+});
