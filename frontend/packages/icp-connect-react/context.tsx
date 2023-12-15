088897e6e7f9f0bdacaf044cf4b7ea664bd25a00
Adrián Darío Hidalgo Flores <ahidalgo@idealizer.mx>
2023-12-14 18:58:14 -0600

    fixes error on set Identity and isAuth state

--- frontend/packages/icp-connect-react/context.tsx
+++ frontend/packages/icp-connect-react/context.tsx
@@ -1,11 +1,9 @@
-import { AnonymousIdentity, Identity } from "@dfinity/agent";
 import { Client } from "icp-connect-core/client";
 import React, { ReactNode, createContext, useEffect, useState } from "react";
 
 export type IcpConnectContextType<T extends Record<string, any>> = {
   client: Client<T>;
   isAuthenticated: boolean;
-  identity: Identity;
   connect: () => Promise<void>;
   disconnect: () => Promise<void>;
 };
@@ -24,28 +22,31 @@ export const IcpConnectContextProvider = <T extends Record<string, any>>({
   // TODO: implement select auth provider
   const [currentAuthProvider, setCurrentAuthProvider] = useState<string | null>("internet-identity");
   const authProvider = currentAuthProvider ? client.getProviders()[currentAuthProvider] : undefined;
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(authProvider?.isAuthenticated() || false);
-  const [identity, setIdentity] = useState<Identity>(authProvider?.getIdentity() || new AnonymousIdentity());
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
-    if (isAuthenticated && !identity.getPrincipal().isAnonymous()) {
+    const identity = authProvider?.getIdentity();
+
+    if (!identity?.getPrincipal().isAnonymous()) {
       client.setIdentity(identity);
-    } else if (!isAuthenticated) {
+      setIsAuthenticated(true);
+    } else {
       setIsAuthenticated(false);
     }
-  }, [isAuthenticated]);
+  }, []);
 
   async function connect() {
     if (!authProvider) throw new Error("No auth provider selected");
 
     await authProvider?.connect();
-    setIdentity(authProvider?.getIdentity());
+    const identity = authProvider?.getIdentity();
+    client.setIdentity(identity);
     setIsAuthenticated(true);
   }
 
   async function disconnect() {
     await authProvider?.disconnect();
-    setIdentity(new AnonymousIdentity());
+    client.setIdentity();
     setIsAuthenticated(false);
   }
 
@@ -54,7 +55,6 @@ export const IcpConnectContextProvider = <T extends Record<string, any>>({
       value={{
         client,
         isAuthenticated,
-        identity,
         connect,
         disconnect,
       }}>
