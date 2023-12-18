9dbbb4695d4b0f8ebd0406e17396e0627745cb59
Alan <alannnc@gmail.com>
2023-12-18 14:21:34 -0700

    Added tailwind, fixed dependencies

--- frontend/apps/internet-identity-middleware/src/components/AuthButton.component.tsx
+++ frontend/apps/internet-identity-middleware/src/components/AuthButton.component.tsx
@@ -1,12 +1,17 @@
 import { useAuth } from "../hooks/useAuth";
+import { Button } from "./ui/button";
 
 const AuthButton = () => {
   const { isAuth, login, navigate } = useAuth();
 
   return !isAuth ? (
-    <button onClick={() => login()}>Ingresar</button>
+    <Button className="text-sm w-full mb-3 bg-blue-600 text-white" onClick={() => login()}>
+      Sign in with Internet Identity
+    </Button>
   ) : (
-    <button onClick={() => navigate()}>Regresar</button>
+    <Button className="text-sm w-full mb-3 bg-blue-600 text-white" onClick={() => navigate()}>
+      Go back
+    </Button>
   );
 };
 
