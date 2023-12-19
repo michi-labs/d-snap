af8faaeb8dd595ff3d908d9a0c3d4e4f14c5c6da
Alan <alannnc@gmail.com>
2023-12-19 08:23:38 -0700

    Wip

--- frontend/apps/mobileapp/index.js
+++ frontend/apps/mobileapp/index.js
@@ -1,10 +1,12 @@
 import "./globals.js";
-import App from "./src/App";
-import { registerRootComponent } from "expo";
-import "fast-text-encoding";
-import "react-native-url-polyfill/auto";
+import "expo-router/entry";
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+// import "react-native-url-polyfill/auto";
+// import App from "./src/App";
+// import { registerRootComponent } from "expo";
+// import "fast-text-encoding";
+
+// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// // It also ensures that whether you load the app in Expo Go or in a native build,
+// // the environment is set up appropriately
+// registerRootComponent(App);
