9953fd21e52cb75c45c18e9761ef079c2bd5fcde
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2023-12-19 21:51:41 -0600

    Update import paths for canisters

--- frontend/apps/webapp/src/pages/_app.tsx
+++ frontend/apps/webapp/src/pages/_app.tsx
@@ -1,22 +1,12 @@
 import "../app/globals.css";
 import { AppLoader } from "../components/app-loader";
 import { AuthContextProvider } from "../lib/auth/auth-context";
-import * as test from "@/declarations/test";
-import * as user from "@/declarations/user";
+import { canisters, CanisterTypes } from "dsnap/lib/canisters";
 import { Client } from "icp-connect-core/client";
 import { InternetIdentity } from "icp-connect-core/identity-providers";
 import { IcpConnectContextProvider } from "icp-connect-react/context";
 import { AppProps } from "next/app";
 import { useEffect, useState } from "react";
-
-const canisters = {
-  test,
-  user,
-};
-
-type CanisterTypes = typeof canisters;
-
-export type ClientCanisterType = Client<CanisterTypes>;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [client, setClient] = useState<Client<CanisterTypes> | undefined>();
@@ -42,6 +32,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }
 
   return client ? (
+    // @ts-ignore
     <IcpConnectContextProvider client={client}>
       <AuthContextProvider>
         <Component {...pageProps} />
