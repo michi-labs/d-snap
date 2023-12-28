064004628459c8a0d5edf6e3c2683cdf72c2e63d
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2023-12-28 15:25:52 -0600

    BigInt support and documents potential polyfill options

--- frontend/apps/mobileapp/shim.js
+++ frontend/apps/mobileapp/shim.js
@@ -1,12 +1,29 @@
-import 'react-native-url-polyfill/auto';
+// import { polyfill as polyfillBase64 } from 'react-native-polyfill-globals/src/base64';
 import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/encoding';
+// import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
 import { polyfill as polyfillURL } from 'react-native-polyfill-globals/src/url';
+// import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fetch';
+// import { polyfill as polyfillCrypto } from 'react-native-polyfill-globals/src/crypto';
 
+// polyfillBase64();
 polyfillEncoding();
+// polyfillReadableStream();
 polyfillURL();
+// polyfillFetch();
+// polyfillCrypto();
 
 /**
  * Polify BigInt.toJSON for react-native
  * @returns {number}
  */
 BigInt.prototype.toJSON = function() { return Number(this.toString()) }
+
+if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
+
+
+// Polyfill for performance.now() on Android
+if (!global.performance && global._chronoNow) {
+    global.performance = {
+      now: global._chronoNow,
+    };
+}
